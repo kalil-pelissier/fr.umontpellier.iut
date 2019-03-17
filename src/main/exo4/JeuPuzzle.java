add55c0b721b4bef812032f40266de06a9f54150
kalil <kalil.pelissier@etu.umontpellier.fr>
2019-03-17 23:50:43 +0100

    mise à jour de la classe hanoi

--- src/main/exo4/JeuPuzzle.java
+++
@@ -1,8 +0,0 @@
-package fr.umontpellier.iut.exo4;
-
-import java.util.ArrayList;
-
-public interface JeuPuzzle {
-    public boolean estGagnant();
-    public ArrayList<JeuPuzzle> genererFils();
-}
--- src/main/java/Hanoi.java
+++ src/main/java/Hanoi.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Hanoi implements JeuPuzzle{
+public class Hanoi implements JeuPuzzle {
     private ArrayList<Integer>[] colonnes;
     private int taille;
 
@@ -13,20 +14,75 @@ public class Hanoi implements JeuPuzzle{
         for(int i=1; i<=taille; i++) {
             this.colonnes[0].add(i);
         }
-
     }
 
-    public boolean estGagnant() {
-        return colonnes[0].isEmpty() && colonnes[1].isEmpty();
+    public Hanoi(ArrayList<Integer>[] colonnes) {
+        this.colonnes = new ArrayList[3];
+        this.colonnes[0] = new ArrayList<>();
+        this.colonnes[1] = new ArrayList<>();
+        this.colonnes[2] = new ArrayList<>();
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j < colonnes[i].size(); j++) {
+                this.colonnes[i].add(colonnes[i].get(j));
+            }
+        }
+        int max1 = 0, max2 = 0, max3 = 0;
+        if (!colonnes[0].isEmpty()){max1 = colonnes[0].get(colonnes[0].size()-1);}
+        if (!colonnes[1].isEmpty()){max2 = colonnes[1].get(colonnes[1].size()-1);}
+        if (!colonnes[2].isEmpty()){max3 = colonnes[2].get(colonnes[2].size()-1);}
+        if (max1>max2){max2 = max1;}
+        if (max2>max3){max3 = max2;}
+        this.taille = max3;
     }
 
+    public Hanoi(Hanoi h){
+        colonnes = new ArrayList[3];
+        this.colonnes[0] = new ArrayList<>();
+        this.colonnes[1] = new ArrayList<>();
+        this.colonnes[2] = new ArrayList<>();
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j < h.colonnes[i].size(); j++) {
+                this.colonnes[i].add(h.colonnes[i].get(j));
+            }
+        }
+        this.taille = h.taille;
+    }
+    
     @Override
-    public String toString() {
-        return "Hanoi{" +
-                "1=" + colonnes[0].toString()+ "\n" +
-                "2=" + colonnes[1].toString()+ "\n" +
-                "3=" + colonnes[2].toString()+
-                '}';
+    public String toString(){
+        String str = "+";
+        for (int i = 0; i < 3*(taille*2-1)+4; i++) {
+            str += "-";
+        }
+        str += "+\n";
+        for (int i = 0; i < taille; i++) {
+            str += "| ";
+            for (int k = 0; k < 3; k++) {
+                if (colonnes[k].size() >= taille - i) {
+                    for (int j = 0; j < taille - colonnes[k].get(i-(taille-colonnes[k].size())); j++) {
+                        str += " ";
+                    }
+                    for (int j = 0; j < 2 * (colonnes[k].get(i-(taille-colonnes[k].size())) - 1) + 1; j++) {
+                        str += "-";
+                    }
+                    for (int j = 0; j < taille - colonnes[k].get(i-(taille-colonnes[k].size())) + 1; j++) {
+                        str += " ";
+                    }
+                }
+                else {
+                    for (int j = 0; j < taille*2; j++) {
+                        str += " ";
+                    }
+                }
+            }
+            str += "|\n";
+        }
+        str += "+";
+        for (int i = 0; i < 3*(taille*2-1)+4; i++) {
+            str += "-";
+        }
+        str += "+";
+        return (str);
     }
 
     public ArrayList<JeuPuzzle> genererFils() {
